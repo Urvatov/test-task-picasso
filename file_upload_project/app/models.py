0425a560453b49f19041ff48a2bafaa69f76ab84
Urvatov <urvatov@gmail.com>
2024-02-22 18:24:38 +0300

    Создана модель File

--- file_upload_project/app/models.py
+++ file_upload_project/app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class File(models.Model):
+    file = models.FileField(upload_to='uploads/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    processed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return str(self.file.name)
+
